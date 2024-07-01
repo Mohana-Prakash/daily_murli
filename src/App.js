1c9ac3863d4f4d101c44965a98e416c694ce7978
Mohana Prakash <mohan@ggexcel.ai>
2024-07-01 14:52:41 +0530

    code initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [content, setContent] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/fetch-murli");
+        setContent(response.data);
+      } catch (error) {
+        console.error("Error fetching the data", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "10px" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div
+          style={{ width: "49%", textAlign: "center" }}
+          dangerouslySetInnerHTML={{ __html: content.tamil }}
+        />
+        <div
+          style={{ width: "49%", textAlign: "center" }}
+          dangerouslySetInnerHTML={{ __html: content.english }}
+        />
+      </div>
     </div>
   );
 }
