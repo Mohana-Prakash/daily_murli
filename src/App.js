9af1a13f846fe2a93152d3b7a5ca105562ee7461
Mohana Prakash <mohan@ggexcel.ai>
2024-09-03 20:29:19 +0530

    live backend url configured

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import moment from "moment";
 function App() {
   const [content, setContent] = useState({});
   const [date, setDate] = useState(moment(new Date()).format("DD.MM.YY"));
+  const [lang, setLang] = useState("tamil");
   const today = new Date().toISOString().split("T")[0];
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:5000/fetch-murli/${date}`
+          `https://daily-murli-backend.vercel.app/fetch-murli/${date}`
         );
         setContent(response.data);
       } catch (error) {}
@@ -22,17 +23,43 @@ function App() {
 
   return (
     <div style={{ padding: "10px" }}>
-      <input
-        type="date"
-        onChange={(e) => setDate(moment(e.target.value).format("DD.MM.YY"))}
-        // value={date}
-        id="dateInput"
-        max={today}
-      />
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div dangerouslySetInnerHTML={{ __html: content.tamil }} />
-        <div dangerouslySetInnerHTML={{ __html: content.english }} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}>
+        <input
+          type="date"
+          onChange={(e) => setDate(moment(e.target.value).format("DD.MM.YY"))}
+          value={date}
+          id="dateInput"
+          max={today}
+          style={{
+            width: "10%",
+            padding: "10px",
+            borderRadius: "5px",
+            border: "solid 1px black",
+          }}
+        />
+        <select
+          style={{ width: "10%", padding: "10px", borderRadius: "5px" }}
+          onChange={(e) => setLang(e.target.value)}>
+          <option value="tamil">Tamil</option>
+          <option value="english">English</option>
+        </select>
       </div>
+      {lang === "tamil" ? (
+        <div
+          style={{ margin: "auto", width: "80%" }}
+          dangerouslySetInnerHTML={{ __html: content.tamil }}
+        />
+      ) : (
+        <div
+          style={{ margin: "auto", width: "80%" }}
+          dangerouslySetInnerHTML={{ __html: content.english }}
+        />
+      )}{" "}
     </div>
   );
 }
